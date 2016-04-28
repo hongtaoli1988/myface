27dad189c5e14c3a637eca55a9f797bb4854021c
unknown <lihongtao@mainone.cn>
2016-04-28 10:34:16 +0800

    bb

---
+++ 存储过程文档整理/ftp/Host文件还原.bat
@@ -0,0 +1,3 @@
+@echo off
+echo 127.0.0.1 localhost > C:\Windows\System32\drivers\etc\hosts
+pause
---
+++ 存储过程文档整理/ftp/下单预发布测试.bat
@@ -0,0 +1,19 @@
+@echo off
+echo.>C:\Windows\System32\drivers\etc\hosts
+echo 211.144.147.199 xd.mymo.cn>> C:\Windows\System32\drivers\etc\hosts
+echo 211.144.147.197 mis.mymo.cn>> C:\Windows\System32\drivers\etc\hosts
+echo 211.144.147.197 zd.mymo.cn>> C:\Windows\System32\drivers\etc\hosts
+echo 211.144.147.197 mis3.mymo.cn>> C:\Windows\System32\drivers\etc\hosts
+echo 211.144.147.254 zdc.b2b.cn>> C:\Windows\System32\drivers\etc\hosts
+echo 211.144.147.194 sfa.mymo.cn>> C:\Windows\System32\drivers\etc\hosts
+echo 211.144.147.12 bb.mymo.cn>> C:\Windows\System32\drivers\etc\hosts
+echo 211.144.147.21 agent.b2b.cn>> C:\Windows\System32\drivers\etc\hosts
+echo 211.144.147.21 agent2.b2b.cn>> C:\Windows\System32\drivers\etc\hosts
+echo 211.144.147.21 am.agent.b2b.cn>> C:\Windows\System32\drivers\etc\hosts
+echo 211.144.147.21 am.agent2.b2b.cn>> C:\Windows\System32\drivers\etc\hosts
+echo 121.40.149.79 www.zhunaw.cc>> C:\Windows\System32\drivers\etc\hosts
+echo 192.168.3.214 mysfa.mymo.cn>> C:\Windows\System32\drivers\etc\hosts
+echo 211.144.147.254 zdc.b2b.cn>> C:\Windows\System32\drivers\etc\hosts
+echo 211.144.147.218 thrm.mymo.cn>> C:\Windows\System32\drivers\etc\hosts
+pause
+
